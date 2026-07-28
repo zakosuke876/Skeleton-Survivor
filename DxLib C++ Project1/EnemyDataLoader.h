e5cf6244eae3d78faba92bc889a85d683f83a8ad
zakosuke876 <7050ha@gmail.com>
2026-07-28 14:17:39 +0900

    使わなくなったクラス・コード削除

--- DxLib C++ Project1/EnemyDataLoader.h
+++ DxLib C++ Project1/EnemyDataLoader.h
@@ -7,17 +7,11 @@
 class EnemyDataLoader {
 public:
 
-	// ¬Œ÷‚µ‚½‚çtrue‚ğ•Ô‚µAoutData‚ÉŒ‹‰Ê‚ğŠi”[‚·‚é
-	//static bool LoadEnemyData(const char* jsonPath, const char* enemyKey, EnemyStatus& outData);
-	//static bool LoadSkeletonWarriorData(const char* jsonPath, SkeletonWarriorStatus& outData);
 	static bool LoadSpawnConfig(const char* jsonPath, EnemySpawnConfig& outData);
 
 	bool LoadNormal(EnemyStatus& outData);
 	bool LoadWarrior(EnemyStatus& outData);
 	bool LoadData(const char* jsonPath, const char* enemyKey, EnemyStatus& outData);
-
-	bool UniqueData();
-	bool LoadWarriorUnique(SkeletonWarriorStatus& outData);
 
 private:
 
